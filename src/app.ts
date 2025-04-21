23cdd92da8ec464b3400ed537084c025b0edce6b
faysal-backend-developer <developer.faysalhasan@gmail.com>
2025-04-21 11:18:49 +0600

    Check All

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'API is working' });
+})
 
 export default app;
