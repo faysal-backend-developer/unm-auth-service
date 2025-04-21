afc3cf96bbe7a232742202c409f040cf4e67257f
faysal-backend-developer <developer.faysalhasan@gmail.com>
2025-04-21 11:23:20 +0600

    Check Husky

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,15 @@ import connectDB from './db';
 
 let server: Server;
 const run = async () => {
-  try {
-    // DB connection
-    await connectDB(config.DB_URL as string);
-    server = app.listen(config.PORT, () => {
-      console.log(`Server is running on port ${config.PORT}`);
-    });
-  } catch (error) {
-    console.error('Error starting server:', error);
-  }
+    try {
+        // DB connection
+        await connectDB(config.DB_URL as string);
+        server = app.listen(config.PORT, () => {
+            console.log(`Server is running on port ${config.PORT}`);
+        });
+    } catch (error) {
+        console.error('Error starting server:', error);
+    }
 };
 
 // এখানে আমরা সার্ভার শুরু করার জন্য একটি ফাংশন তৈরি করেছি যা সার্ভার শুরু করতে ব্যবহৃত হবে। এখানে সার্ভার দিয়ে আর কি কি কাজ করা যায় তা জানতে হবে ।
